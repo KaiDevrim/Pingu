00542981cf237bed4897b941c107156501e245ce
Kai Devrim <kai@devrim.tech>
2022-05-13 11:51:42 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+    private DiscordSocketClient _client;
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+        string token = File.ReadAllText(".env");
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
