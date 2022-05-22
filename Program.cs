aca67d4d9750948803723fc91fb76ca6cc0960e4
Kai Devrim <kai@devrim.tech>
2022-05-22 15:38:43 -0700

    added ayush shrimp spam

--- Program.cs
+++ Program.cs
@@ -19,7 +19,8 @@ public class Program
             Intents = DiscordIntents.All
         });
         discord.GuildMemberAdded += MemberAddedHandler;
-        var slash = discord.UseSlashCommands();
+        discord.MessageCreated += AyushMessage;
+            var slash = discord.UseSlashCommands();
         slash.RegisterCommands<RoleCommands>();
         await discord.ConnectAsync();
         await Task.Delay(-1);
@@ -28,5 +29,38 @@ public class Program
     private static async Task MemberAddedHandler(DiscordClient s, GuildMemberAddEventArgs e)
     {
         await e.Member.GrantRoleAsync(e.Guild.Roles.Values.FirstOrDefault(x => x.Id == 974526234086232104));
+    }
+
+    private static async Task AyushMessage(DiscordClient s, MessageCreateEventArgs e)
+    {
+        string[] EmojiNames =
+        {
+            ":shrimp:",
+            ":fried_shrimp:",
+            ":GemShrimp:",
+            ":PepeShrimp:",
+            ":WumpusShrimp:",
+            ":goldshrimp:",
+            ":shrimp_1:",
+            ":shrimp_2:",
+            ":shrimp_3:",
+            ":shrimp_4:",
+            ":shrimp_5:",
+            ":shrimp_6:",
+            ":shrimp_7:",
+            ":shrimp_8:",
+            ":shrimp_9:",
+            ":shrimp_10:",
+            ":shrimp_11:",
+
+        };
+        if (e.Author.Id == 589611658087891002)
+        {
+            foreach (string emoji in EmojiNames)
+            {
+                await Task.Delay(200);
+                await e.Message.CreateReactionAsync(DiscordEmoji.FromName(s,emoji));
+            }
+        }
     }
 }
